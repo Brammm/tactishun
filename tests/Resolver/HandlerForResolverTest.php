c25f8fabb1db1bb36c3789b4bc7a085bc28b18cb
Adrian <adrian.testaavila@gmail.com>
2025-08-05 19:10:40 -0400

    add #[HandlerFor] attribute add HandlerForResolver add tests

---
+++ tests/Resolver/HandlerForResolverTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brammm\Tactishun\Tests\Resolver;
+
+use Brammm\Tactishun\HandlerForResolver;
+use Brammm\Tactishun\Resolver\HandlerNotFound;
+use Brammm\Tactishun\Tests\TestImplementations\Command;
+use Brammm\Tactishun\Tests\TestImplementations\CommandHandler;
+use Brammm\Tactishun\Tests\TestImplementations\CommandHandlerFor;
+use Brammm\Tactishun\Tests\TestImplementations\CommandWithoutAttribute;
+use PHPUnit\Framework\TestCase;
+
+final class HandlerForResolverTest extends TestCase
+{
+    public function testItResolvesRegisteredCommand(): void
+    {
+        $resolver = new HandlerForResolver(CommandHandlerFor::class);
+        $handler  = $resolver->resolve(new CommandWithoutAttribute('Jerry'));
+
+        self::assertEquals(CommandHandlerFor::class, $handler);
+    }
+
+    public function testItThrowsExceptionIfCommandNotRegistered(): void
+    {
+        $resolver = new HandlerForResolver(CommandHandlerFor::class);
+
+        $this->expectException(HandlerNotFound::class);
+        $resolver->resolve(new Command('Jerry'));
+    }
+}
